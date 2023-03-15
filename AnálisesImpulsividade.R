3246dd0be4a808b980dbf7061704f59db9144d97
Eddac <cordeiro.emanuel@gmail.com>
2023-03-15 11:05:41 -0300

    Ufpe

--- AnálisesImpulsividade.R
+++ AnálisesImpulsividade.R
@@ -9,6 +9,16 @@ library(readxl)
 df <- read_csv("Construção de Instrumentos 2022.2 (respostas) - Respostas ao formulário 1-3.csv")
 df <- read_xlsx("Construção de Instrumentos 2022.2 (respostas).xlsx")
 
+# Renomear
+df <- df %>% rename(TCLE = "Tendo em vista os itens acima apresentados, eu, de forma livre e esclarecida, manifesto meu consentimento para participar da pesquisa.")
+
+# TCLE
+df$TCLE[df$TCLE == 'Concordo'] <- '1'
+df$TCLE[df$TCLE == 'Não concordo'] <- '0'
+
+# Eliminação de negativos
+df <- df %>% filter(TCLE==1) # Eliminação de não respondentes
+
 # saparando intrumetnos de impulsividade
 df_impulsividade <- df[, 10:51] 
 
@@ -86,3 +96,51 @@ numeros <- as.integer(str_extract_all(str, "(?<=impul)\\d+"))
 
 # imprimindo a lista de números inteiros
 print(numeros)
+
+
+# 
+
+# AFE_2 -------------------------------------------------------------------
+
+# Paralell analysis
+
+poly_im <- df_impulsividade %>% polychoric(.)   # matriz de correlações policóricas para análise paralela.
+rho_im <- poly_im$rho # guardando apenas o Rho de Spearman da matriz de correlações policóricas
+scree_im<-scree(poly_im)
+
+pa_R <- fa.parallel(rho_im, n.obs=830, fa="fa") # análise paralela sobre a matriz de correlações policóricas
+pa_R$fa.values #mostra os eigenvalues com os dados experimentais
+pa_R$fa.sim #mostra os eigenvalues com os dados simulados
+
+# OBS.: a análise paralela indicou 6 fatores, o scree-plot 3 ou 4 fatores,
+# e pelo critério de Kaiser-Guttman (eigenvalue > 1), formariam-se quatro fatores.
+# Assim, optou-se pela extração de 4 fatores.
+
+# Fatores
+names(R_2)
+r_efa <- fa(R, nfactors = '4', cor='poly', 
+            fm='wls', rotate = 'geominQ')
+# nessa analise, o item 7 apresentou cargas inferiores a 0,3 em todos os fatores.
+
+R <- R[ ,c(1:6,8:24)]
+
+r_efa <- fa(R, nfactors = '4', cor='poly', 
+            fm='wls', rotate = 'geominQ')
+
+r_efa$e.values
+# resultados em tabelas
+names (r_efa)
+r_efa$loadings
+
+#install.packages("DT")  # pacote para fazer tabelas.
+#library(DT)
+#view(R)
+
+#datatable(r_itens$loadings[1:23, 1:3], rownames=TRUE, editable = TRUE) 
+
+library(knitr)  #pacote para tabelas
+kable(r_efa$loadings[1:23, 1:4],digits = 2)
+
+#R-Fidedignidade
+#names(riasec_2)
+#riasec_2 %>% select(24:29,36,41:43,45) %>% alpha()
