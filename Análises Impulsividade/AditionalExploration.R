b5c5329375babceb68af9730fc17d2a2ca583cc6
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-04-15 20:22:46 -0300

    aditional exploration first order done

--- Análises Impulsividade/AditionalExploration.R
+++ Análises Impulsividade/AditionalExploration.R
@@ -6,10 +6,8 @@ library(tidyverse)
 library(readxl)
 
 df <- read_xlsx("Base de dados/Construção de Instrumentos 2022.2 (respostas).xlsx")
-df <- df %>% rename(TCLE = "Tendo em vista os itens acima apresentados, eu, de forma livre e esclarecida, manifesto meu consentimento para participar da pesquisa.")
-df$TCLE[df$TCLE == 'Concordo'] <- '1'
-df$TCLE[df$TCLE == 'Não concordo'] <- '0'
-df <- df %>% filter(TCLE==1) # Eliminação de não respondentes
+# removendo quem negou o TCLE
+df <- na.omit(df)
 
 # saparando intrumetnos de impulsividade
 df_impulsividade <- df[, 10:51]
